83d1a22c029454db2c36040b624cb55ed3aa3ec7
Uzair <uzair.z.shaikh@gmail.com>
2021-01-22 19:33:26 +0000

    island class added jittery random movement added when player on island break added when collide with player and zombie

--- venv/ZombieTag.py
+++ venv/ZombieTag.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-
+import random
 
 width, height=1500,700
 colour =(0,0,0)
@@ -58,40 +58,67 @@ class Zombie:
             self.y += self.speed
         self.rect = pygame.Rect( self.x, self.y , self.size, self.size)
 
+    def randommove(self):
+        self.x -= random.randint(0, 2)
+        self.y +=random.randint(0,2)
+        self.x += random.randint(0, 2)
+        self.y -= random.randint(0, 2)
+        self.rect = pygame.Rect( self.x, self.y , self.size, self.size)
+
 
 class Island:
-    def __init__(self,size, colour):
-        self.size  = size
+    def __init__(self,size, colour, startingx , startingy):
+        self.size = size
         self.colour = colour
+        self.x = startingx
+        self.y = startingy
+        self.rect = pygame.Rect( self.x, self.y , self.size, self.size)
     
 def draw_player(Player):
     pygame.draw.rect(window, Player.colour,(Player.rect))
 
-def draw_window(Playe,Zombi):
+def draw_window(Play,Zomb,land,time):
     window.fill(colour)
-    draw_player(Zombi)
-    draw_player(Playe)
+    if int(time) < 5:
+        pygame.draw.rect(window, land.colour, (land.rect))
+    draw_player(Zomb)
+    draw_player(Play)
 
     pygame.display.update()
 
 
 def main():
     clock = pygame.time.Clock()
-    Play = Player(0,0,20,1,(255,7,6))
-    Zomb = Zombie(200, 100, 20, 1, (200, 7, 200))
+    time = 0
+    Play = Player(0,0,20,2,(255,7,6))
+    Zomb = Zombie(900, 600, 20, 1.5, (200, 7, 200))
+    land = Island(200,(0,250,0), 200,200)
     while True:
         clock.tick(fps)
+        if int(time)==10:
+            time=0
+            landx=random.randint(0,width-land.size)
+            landy=random.randint(0,height-land.size)
+            land= Island(200,(0,250,0), landx,landy)
+        else:
+            time += 1/60
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
         keys_pressed = pygame.key.get_pressed()
 
-
         Play.move(keys_pressed)
-        Zomb.chase(Play)
+        if Play.rect.colliderect(land) and int(time)<5:
+            Zomb.randommove()
+        else:
+            Zomb.chase(Play)
 
-        draw_window(Play, Zomb)
+        if Play.rect.colliderect(Zomb):
+            break
+
+        draw_window(Play, Zomb,land,time)
 
 
 
